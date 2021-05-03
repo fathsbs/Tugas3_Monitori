22a03d14a4ba7737b435c1bb0f9ddaa23704790a
fathsbs <fatahsbs@gmail.com>
2021-05-03 21:55:44 +0700

    pembuatan activity_login

--- app/src/main/java/com/fathsbs/monitori/welcomeActivity.java
+++ app/src/main/java/com/fathsbs/monitori/welcomeActivity.java
@@ -1,4 +1,15 @@
 package com.fathsbs.monitori;
 
-public class welcomeActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class welcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
 }
